e98b7e932efeb9a8d482cd40bd3b436151a15a07
Randy Moreno <126211140+Nito1996@users.noreply.github.com>
2024-07-08 00:30:57 -0400

    Added via upload.
    Added initial structure for PERMISSION-MANAGER repository:
    - Implemented CRUD operations for managing permission requests.
    - Included user input validation for name, last name, and permission type.
    - Enhanced user interface with clear prompts and feedback messages.
    - Integrated Enum for PermissionType to categorize requests effectively.

---
+++ PermissionManager/PermissionOperation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Security;
+
+namespace PermissionManager
+{
+    public interface IPermissionOperation
+    {
+        public void Create(Permission permission);
+        public IList<Permission> Read();
+        public void Update(int index, Permission permission);
+        public void Delete(int index);
+
+    }
+    public class PermissionOperation : IPermissionOperation
+    {
+        public static IList<Permission> permissionsList = new List<Permission>();
+        public void Create(Permission permission)
+        {
+            permissionsList.Add(permission);
+        }
+        public IList<Permission> Read()
+        {
+            return permissionsList;
+        }
+        public void Update(int index, Permission permission)
+        {
+            permissionsList[index] = permission;
+        }
+        public void Delete(int index)
+        {
+            permissionsList.RemoveAt(index);
+        }
+    }
+}
